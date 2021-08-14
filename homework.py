865f31aecc79b33a54c9b94c67e109da449dbbbd
Inna <innavalovamail@gmail.com>
2021-08-14 12:29:12 +0300

    Правки для PEP8 в исполняемом коде

--- homework.py
+++ homework.py
@@ -112,18 +112,15 @@ if __name__ == "__main__":
     # дата в параметрах не указана,
     # так что по умолчанию к записи
     # должна автоматически добавиться сегодняшняя дата
-    cash_calculator.add_record(Record(
-                                    amount=145,
-                                    comment='кофе'))
+    cash_calculator.add_record(Record(amount=145,
+                                      comment='кофе'))
     # и к этой записи тоже дата должна добавиться автоматически
-    cash_calculator.add_record(Record(
-                                    amount=300,
-                                    comment='Серёге за обед'))
+    cash_calculator.add_record(Record(amount=300,
+                                      comment='Серёге за обед'))
     # а тут пользователь указал дату, сохраняем её
-    cash_calculator.add_record(Record(
-                                    amount=3000,
-                                    comment='бар в Танин др',
-                                    date='08.11.2019'))
+    cash_calculator.add_record(Record(amount=3000,
+                                      comment='бар в Танин др',
+                                      date='08.11.2019'))
     print(cash_calculator.get_today_cash_remained('rub'))
     # должно напечататься
     # На сегодня осталось 555 руб
